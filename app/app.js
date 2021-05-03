3c90eacf07fc97474f1b8dde240ab127f0441732
susukida <susukida@loremipsum.co.jp>
2021-05-03 11:47:22 +0900

    add home page

---
+++ app/app.js
@@ -0,0 +1,62 @@
+const path = require('path')
+const morgan = require('morgan')
+const winston = require('winston')
+const express = require('express')
+const helmet = require('helmet')
+
+class App {
+  constructor () {
+    this.router = express()
+
+    this.router.set('strict routing', true)
+    this.router.set('views', path.join(__dirname, 'view'))
+    this.router.set('view engine', 'pug')
+    this.router.set('trust proxy', true)
+
+    this.router.use(helmet())
+    this.router.use(morgan(process.env.LOG_ACCESS, {
+      stream: {
+        write: message => {
+          winston.loggers.get('access').info(message.trim())
+        },
+      },
+    }))
+
+    this.router.use(this.onRequestInitialize.bind(this))
+    this.router.use('/static/', express.static(path.join(__dirname, 'static')))
+
+    this.router.get('/', (req, res) => res.render('home'))
+    this.router.use(this.onNotFound.bind(this))
+    this.router.use(this.onInternalServerError.bind(this))
+  }
+
+  onListening () {
+    winston.loggers.get('info').info(`Listening on ${process.env.PORT}`)
+  }
+
+  onRequest (req, res) {
+    this.router(req, res)
+  }
+
+  onRequestInitialize (req, res, next) {
+    res.locals.env = process.env
+
+    next()
+  }
+
+  onNotFound (req, res) {
+    res.status(404).end()
+  }
+
+  onInternalServerError (err, req, res, next) {
+    res.status(500).end()
+    this.onError(err)
+  }
+
+  onError (err) {
+    winston.loggers.get('error').error(err.message)
+    winston.loggers.get('debug').debug(err.stack)
+  }
+}
+
+module.exports.App = App
